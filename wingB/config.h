cddab3063c3edef7b9456c5a674b7f9f060e67ba
mastery6 <91782246+mastery6@users.noreply.github.com>
2021-10-02 19:12:07 +0900

    wingB_code

---
+++ wingB/config.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6363
+#define DEVICE_VER      0x0001
+#define PRODUCT         WingB
+#define MANUFACTURER    Mastery
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 6
+
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
+
+// wiring of each half
+
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5}
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6}
+
+#define DIODE_DIRECTION COL2ROW
+#define SOFT_SERIAL_PIN D1
+
+// WS2812 RGB LED strip input and number of LEDs
+// #define RGB_DI_PIN D3
+// #define RGBLED_NUM 12
---
+++ wingB/keymaps/default/keymap.c
@@ -0,0 +1,42 @@
+#include QMK_KEYBOARD_H
+#include "wingB.h"
+
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+     KC_GRV ,    KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9   , KC_0    ,KC_EQL,
+     KC_TAB ,    KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O   , KC_P    ,KC_MINS,
+     MO(_LOWER), KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L   , KC_SCLN ,KC_QUOT,
+     KC_LSFT ,   KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_B  , KC_N  , KC_M  , KC_COMM, KC_DOT  ,KC_SLSH,
+     KC_ESC ,    KC_LCTL , KC_CAPS , KC_LOPT, KC_LCMD,KC_SPC,                  KC_SPC , KC_RCMD , KC_LEFT ,KC_DOWN ,KC_UP ,KC_RGHT ,
+                                      KC_MUTE , KC_ENT, KC_BSPC,                 KC_BSPC, KC_ENT, KC_MUTE
+  ),
+  [_LOWER] = LAYOUT(
+     KC_F1 , KC_F2 ,  KC_F3  , KC_F4 , KC_F5 , KC_F6 ,                      KC_F7,   KC_F8 , KC_F9 , KC_F10 ,KC_F11 ,KC_F12 ,
+     KC_TILD,KC_ESC, KC_UP ,KC_RGHT,KC_LBRC ,KC_PERC,                        KC_RPRN, KC_RBRC  , KC_7  , KC_8   ,KC_9   ,KC_DEL,
+     _______,KC_LEFT,KC_DOWN,KC_RGHT,KC_LCBR,KC_LPRN,                        KC_RPRN, KC_RCBR  , KC_4  , KC_5   ,KC_6   ,KC_PLUS,
+     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_1  , KC_1  , KC_2   ,KC_3   ,KC_PIPE,
+     _______,_______,_______,_______,_______,_______,                        KC_0,    KC_0  , KC_P2 , KC_P3  ,KC_EQL ,KC_UNDS,
+                            _______,KC_PSCR, _______,                       _______, KC_P0, _______
+
+  )
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	return true;
+}
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+      tap_code(KC_VOLD);
+    } else {
+      tap_code(KC_VOLU);
+    }
+    return true;
+}

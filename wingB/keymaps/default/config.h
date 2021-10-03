0cb2f207901a7ee922d307180cc0f985af83a27e
mastery <91782246+mastery6@users.noreply.github.com>
2021-10-03 23:02:08 +0900

    Update config.h

--- wingB/keymaps/default/config.h
+++ wingB/keymaps/default/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 
 /* Select hand configuration */
-//#define MASTER_LEFT
-#define MASTER_RIGHT
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 // #define EE_HANDS
 
 // #undef RGBLED_NUM

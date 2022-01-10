8d2e3cea632e3666be04e4f2256a6711db28c37e
mastery <91782246+mastery6@users.noreply.github.com>
2022-01-10 16:53:57 +0900

    Add files via upload

--- wingB/rules.mk
+++ wingB/rules.mk
@@ -18,6 +18,6 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 SPLIT_KEYBOARD = yes
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = no
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
